f35375bb1fda0ef8a020922c62afcfd8a118f179
McKinley Fox <mckinleyfox@148-100-150-120.FoxNet.marist.edu>
2019-02-08 15:25:03 -0500

    Added goodbye message to Hello program

--- hello.py
+++ hello.py
@@ -4,3 +4,11 @@
 def main():
    print("Hello, instructor!")
 main()
+
+# Introduction to Programming
+# Author: your name here
+# Date: today's date here
+def main():
+    print("Hello, instructor!")
+    print("Good-bye!")
+main()
