81b66428670eeb343ccd55788411495583c33c7c
McKinley Fox <mckinleyfox@148-100-150-120.FoxNet.marist.edu>
2019-03-01 18:29:33 -0500

    User's choice of function

--- calc_functions.py
+++ calc_functions.py
@@ -19,28 +19,12 @@ def addition(x, y):
 def subtraction(x, y):
    return x - y
 
+#gives the user a choice of their function
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
-# Take input from the user 
-choice = input("Enter choice(1/2/3/4):")
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
 
-if choice == '1':
-   print(num1,"+",num2,"=", add(num1,num2))
-
-elif choice == '2':
-   print(num1,"-",num2,"=", subtract(num1,num2))
-
-elif choice == '3':
-   print(num1,"*",num2,"=", multiply(num1,num2))
-
-elif choice == '4':
-   print(num1,"/",num2,"=", divide(num1,num2))
-else:
-   print("Invalid input")
