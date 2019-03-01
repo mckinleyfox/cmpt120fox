00e2d8c1503c653d695580cf6fd98e812b6b43c2
McKinley Fox <mckinleyfox@148-100-150-120.FoxNet.marist.edu>
2019-03-01 17:56:57 -0500

    Final changes to Marist Username and Password

--- credentials.py
+++ credentials.py
@@ -1,7 +1,7 @@
 # CMPT 120 Intro to Programming
 # Lab #5 – Working with Strings and Functions
-# Author: Your Name Here
-     # Created: YYYY-MM-DD
+# Author: McKinley Fox
+     # Created: 2019-02-25
 def getName():
     first = input("Enter your first name: ")
     last = input("Enter your last name: ")
@@ -13,12 +13,30 @@ def uname(name):
 def password():
     passwd = input("Create a new password: ")
     # TODO modify this to ensure the password has at least 8 character
-    while (len(passwd)<8):
-        print("Fool of a Took! That password is feeble!")
+    #while (len(passwd)<8):
+        #print("Fool of a Took! That password is feeble!")
+        #passwd = input("Create a new password: ")
+    #print("The force is strong in this one...")
+    weak = "weak"
+    med = "medium"
+    strong = "strong"
+    if len(passwd) > 8:
+        print ("password is", strong)
+    elif len(passwd) < 8:
+        print("password is", weak)
         passwd = input("Create a new password: ")
-    print("The force is strong in this one...")
+    if passwd.lower()== passwd or passwd.upper()==passwd:
+        print ('password is', weak)
+        passwd = input("Create a new password: ")
+    elif passwd.lower()== passwd and passwd.upper()==passwd:
+        print ('password is', med)
+    else:
+        passwd.lower()== passwd and passwd.upper()==passwd
+        print ('password is', strong)
     return passwd
-        
+
+    
+
 def main():
     # get user's first and last name
     name= getName()
@@ -27,5 +45,5 @@ def main():
     # TODO modify this to generate a Marist-style username
     # ask user to create a new password
     
-    print("Account configured. Your new email address is", user + "@marist.edu")
+    print("Account configured. Your new email address is", user.lower() + "@marist.edu")
 main()
