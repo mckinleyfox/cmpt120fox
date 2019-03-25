c0d84a140fada4d3675db5f6b7074eabb15a7b8c
McKinley Fox <mckinleyfox@148-100-150-120.FoxNet.marist.edu>
2019-03-25 17:56:21 -0400

    Usability improvements to the guessing game, resolves #2 and #3

--- guessing-game.py
+++ guessing-game.py
@@ -1,14 +1,16 @@
 #This lab involves conditional excecution and strings
 
 def main():
-    print("The program is thinking of an animal.")
+    actual = "dolphin"
     while True:
-        guess = input("Guess the name of the animal. ")
-        actual = "dolphin"
+        print("The program is thinking of an animal.")
+        guess = input("Guess the name of the animal. ").lower()
         if guess == actual:
             print("You are correct! Congrats!")
             break
+        elif guess == "quit":
+            print("Game over")
+            break
         else:
             print("This answer is incorrect, try again.")
-
 main()
