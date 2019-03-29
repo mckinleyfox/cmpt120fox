eb3d8db31bc1363dcfbcc22f9f16c67770c90802
McKinley Fox <mckinleyfox@148-100-150-120.FoxNet.marist.edu>
2019-03-29 15:41:15 -0400

    Usability improvements to the guessing game, resolves #$ and #5

--- guessing-game.py
+++ guessing-game.py
@@ -5,11 +5,16 @@ def main():
     while True:
         print("The program is thinking of an animal.")
         guess = input("Guess the name of the animal. ").lower()
-        if guess == actual:
-            print("You are correct! Congrats!")
-            break
-        elif guess == "quit":
+        if guess[0] == "q" :
             print("Game over")
+            break
+        elif guess == actual:
+            print("You are correct! Congrats!")
+            like = input("Do you like dolphins?(y=yes and n=no)")
+            if like == "y":
+                print("Yay! That is awesome!")
+            elif like == "n":
+                print("Aw man! It's my favorite animal!")
             break
         else:
             print("This answer is incorrect, try again.")
