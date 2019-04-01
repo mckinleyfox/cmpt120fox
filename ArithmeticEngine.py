2019b46a2fe380805787e371a0406823e2996833
McKinley Fox <mckinleyfox@148-100-150-120.FoxNet.marist.edu>
2019-04-01 17:48:33 -0400

    mix-case and invalid commands

---
+++ ArithmeticEngine.py
@@ -0,0 +1,41 @@
+# CMPT 120 - Lab #6
+# McKinley Fox
+# 04-1-2019
+###
+
+def showIntro():
+    print("Welcome to the Arithmetic Engine!")
+    print("=================================\n")
+    print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+    
+def showOutro():
+    print("\nThank you for using the Arithmetic Engine...")
+    print("\nPlease come back again soon!")
+    
+def doLoop():
+    while True:
+        cmd = input("What computation do you want to perform? ").lower() 
+        if cmd in ["add", "sub", "mult", "div"]:
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input("Enter the second number: "))
+        if cmd == "add":
+            result = num1 + num2
+        elif cmd == "sub":
+            result = num1 - num2
+        elif cmd == "mult":
+            result = num1 * num2
+        elif cmd == "div":
+            result = num1 // num2
+        elif cmd == "quit":
+            break
+        else:
+            print(cmd, "is not a valid command")
+            break
+        print("The result is " + str(result) + ".\n") 
+        
+def main():
+    showIntro()
+    doLoop()
+    showOutro()
+    
+main()
